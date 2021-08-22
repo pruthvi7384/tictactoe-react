843726344614dd54d31eeadbe4cee1596b96f208
pruthvi7384 <pruthvirajrajput575@gmail.com>
2021-08-22 14:23:52 +0530

    deployment phase

--- src/componants/StaticMessage.js
+++ src/componants/StaticMessage.js
@@ -8,7 +8,7 @@ function StaticMessage({winner,current}) {
     const noMoveLeft = current.board.every(el => el !==null);
     return (
         <div className="status-message">
-            {winner && !noMoveLeft && <>Winner is <span className={winner === 'X' ? 'text-green' : 'text-orange'}>${winner} !</span></>}{!winner && <>Next player is <span  className={winner === 'X' ? 'text-green' : 'text-orange'}>{current.isXNext ? 'X' : 'O'} !</span></>}{!winner && noMoveLeft && <> <span className="text-green">X</span> <span className="text-orange">O</span> tied !</>}
+            {winner && !noMoveLeft && <>Winner is <span className={winner === 'X' ? 'text-green' : 'text-orange'}>{winner} !</span></>}{!winner && <>Next player is <span  className={winner === 'X' ? 'text-green' : 'text-orange'}>{current.isXNext ? 'X' : 'O'} !</span></>}{!winner && noMoveLeft && <> <span className="text-green">X</span> <span className="text-orange">O</span> tied !</>}
         </div>
     )
 }
